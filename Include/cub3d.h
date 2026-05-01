4e8a90ff2cd72905fcd4cac476b29828a8101837
Motaz9999 <motasjamal246@gmail.com>
2026-05-01 21:11:56 +0300

    Implement libft and add main.c for  purposes.

---
+++ Include/cub3d.h
@@ -0,0 +1,10 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+# include "libft.h"
+# include <limits.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# define TRUE 1
+# define FALSE 0
+#endif

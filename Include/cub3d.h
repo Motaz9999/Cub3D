2d307d8b98b49e2f306f662aabae1c8a1025ae3a
Motaz9999 <motasjamal246@gmail.com>
2026-05-01 22:00:21 +0300

    Add player, config, and game structures; update main function

--- Include/cub3d.h
+++ Include/cub3d.h
@@ -1,9 +1,11 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 # include "libft.h"
+# include <error.h>
 # include <limits.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <time.h>
 # include <unistd.h>
 # define TRUE 1
 # define FALSE 0
--- Include/struct.h
+++ Include/struct.h
@@ -1,4 +1,18 @@
 #ifndef STRUCT_H
 # define STRUCT_H
+typedef struct s_player
+{
+	int			x;
+	int			y;
+}				t_player;
+typedef struct s_config
+{
+}				t_config;
+
+typedef struct s_game
+{
+	t_config	**map;
+	t_player	*player;
+}				t_game;
 
 #endif
